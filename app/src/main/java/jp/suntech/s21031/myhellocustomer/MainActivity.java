64756bdfe76c1aed0a88212246e047060e800f7c
stc-s21031 <s21031@suntech.jp>
2022-06-28 12:00:33 +0900

    commit

--- app/src/main/java/jp/suntech/s21031/myhellocustomer/MainActivity.java
+++ app/src/main/java/jp/suntech/s21031/myhellocustomer/MainActivity.java
@@ -9,7 +9,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-//retry
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
